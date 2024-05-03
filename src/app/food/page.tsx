c05b1cc0b9b8a8bf3dde9e1c277caf2c3a307f24
Khai Fahmi Zaki <khaifahmi99@gmail.com>
2024-05-03 23:12:54 +1000

    feat: added metadata to food grid

--- src/app/food/page.tsx
+++ src/app/food/page.tsx
@@ -6,23 +6,34 @@ import { getTotalFoods } from '@/lib/api';
 import Pagination from '../_components/pagination';
 import { Food } from '@/interfaces/food';
 
+interface FoodWithImage {
+  food: Food,
+  image: ImageProps;
+}
+
 export default async function Foods({ searchParams }: Props) {
   const page = Number(searchParams?.page) || 1;
   const totalFoods = await getTotalFoods();
 
   const hasNextPage = page * FOOD_PAGE_SIZE < totalFoods;
 
-  const props = await getImages(page);
+  const props = await getFoods(page);
 
   const url = "https://d3ae3kedxtitrj.cloudfront.net/food/";
 
-  const images: ImageProps[] = props.map((image, i) => ({
-    id: `${i}`,
-    src: `${url}${image}`,
-    alt: "sketch",
-    public_id: image,
-    height: 512,
-    width: 512,
+  const foods: FoodWithImage[] = props.map((food, i) => ({
+    image: {
+      id: `${i}`,
+      src: `${url}${food.images}`,
+      alt: "sketch",
+      public_id: food.images,
+      height: 512,
+      width: 512,
+    },
+    food: {
+      ...food,
+      images: [food.images],
+    },
   }));
 
   return (
@@ -31,14 +42,34 @@ export default async function Foods({ searchParams }: Props) {
         <Header category='food'/>
         <article className="mb-32">
           <div className="grid gap-4 grid-cols-1 sm:grid-cols-3 mb-16">
-            {images.map((image) => (
-              <Image
-                alt={image.alt}
-                className="hover:scale-110 transition duration-200 ease-in-out 
-                  rounded-lg mb-2"
-                src={image.src}
-                width={image.width}
-                height={image.height} />
+            {foods.map(({food, image}) => (
+              <div className='relative'>
+                <Image
+                  alt={image.alt}
+                  className="rounded-lg mb-2 hover:brightness-50"
+                  src={image.src}
+                  width={image.width}
+                  height={image.height} 
+                />
+                <div className='absolute top-2 right-2 bg-opacity-50 bg-indigo-700 rounded-lg px-2 text-white cursor-default pointer-events-none'>
+                  📸 {food.capturedOn}
+                </div>
+                <div className='main-section absolute bottom-0 inset-x-0 h-32 text-white pointer-events-none'>
+                  <div className='p-2 truncate'>
+                    <div className='main-header pb-4'>
+                      <div className='text-lg font-bold'>
+                          {food.restaurantName ? `📍 ${food.restaurantName}` : '🏪🧑‍🍳'}
+                      </div>
+                      <div className='italic'>
+                        {food.city}, {food.country}
+                      </div>
+                    </div>
+                    <div className='text-wrap'>
+                      {food.description}
+                    </div>
+                  </div>
+                </div>
+              </div>
             ))}
           </div>
           <Pagination page={page} hasPreviousPage={page > 1} hasNextPage={hasNextPage} />
@@ -48,7 +79,7 @@ export default async function Foods({ searchParams }: Props) {
   );
 }
 
-const getImages = async (page = 1) => {
+const getFoods = async (page = 1) => {
   const res = await fetch(`https://inference-logs.khaifahmi99.workers.dev/food?page=${page}&pageSize=${FOOD_PAGE_SIZE}`, {
     cache: 'no-store',
   })
@@ -58,9 +89,13 @@ const getImages = async (page = 1) => {
   }
  
   const foodRecords: Food[] = await res.json();
-  const images = foodRecords.map(food => food.images[0]);
+  return foodRecords.map(food => ({
+    ...food,
+    images: food.images[0],
+    // FIXME: The DB should not store 'nan'
+    restaurantName: food.restaurantName === 'nan' ? undefined : food.restaurantName,
+  }))
 
-  return images
 }
 
 interface Props {
